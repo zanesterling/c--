f933a587b429b652d1ff51b819e6278ba2775738
Severyn-Kozak <severyn.kozak@gmail.com>
2013-05-30 18:52:18 -0400

    Edited Thing.

--- Thing.java
+++ Thing.java
@@ -1,9 +1,12 @@
-public class Thing {
+public abstract class Thing {
 
 	double x,y;
 	double radius;
 
-	public void collide(Actor a) {
-
+	public void collide(Actor act) {
+    if(Math.sqrt(Math.pow(x - act.x, 2) + Math.pow(y - act.y, 2)) < radius ) //compares this thing's radius to distance between itself and act
+		thingEffect(act);
 	}
+	
+	abstract void thingEffect(Actor act);	//effect of Thing's subclasses 
 }
