60b246144bb6b671294663f977a2abe95693378f
Severyn-Kozak <severyn.kozak@gmail.com>
2013-05-31 13:29:10 -0400

    Added constructor to Thing.

--- Thing.java
+++ Thing.java
@@ -3,8 +3,16 @@ public abstract class Thing {
 	double x,y;
 	double radius;
 
+	BufferedImage img;
+
+	public Thing(int x, int y, String imgpath) {
+		this.x = x;
+		this.y = y;
+		img = Util.loadImg(imgpath);
+	}     
+	
 	public void collide(Actor act) {
-    if(Math.sqrt(Math.pow(x - act.x, 2) + Math.pow(y - act.y, 2)) < radius ) //compares this thing's radius to distance between itself and act
+		if(Math.sqrt(Math.pow(x - act.x, 2) + Math.pow(y - act.y, 2)) < radius ) //compares this thing's radius to distance between itself and act
 		thingEffect(act);
 	}
 	
