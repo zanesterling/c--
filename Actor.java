9fef2bf66fcafc4c442b9be35d72570c2f4cfde7
Sebastian Conybeare <sebmathguy@gmail.com>
2013-05-31 16:45:33 -0400

    Add comments to Actor

--- Actor.java
+++ Actor.java
@@ -7,8 +7,8 @@ public abstract class Actor {
 	
 	//coordinates
 	double x, y;
-	double vx, vy, vt;
-	double dt;
+	double vx, vy, vt; //vx and vy are about velocity in the obvious way, vt is a measure of how much of the player's t coordinate is observed to elapse per unit time in the actor's reference frame
+	double dt; //this is a correction term used to measure how far off the actor's clock is from where it should be; in an ideal simulation it would be 0.
 	
 	Weapon wep;
 
