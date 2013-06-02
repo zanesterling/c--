cb704092a8d29826490906fceeb9019bf6b96ada
Zane Sterling <sterling.zane@gmail.com>
2013-06-02 14:48:50 -0400

    Remove dt from Actor.

--- Actor.java
+++ Actor.java
@@ -6,7 +6,6 @@ public abstract class Actor {
 	double x, y;
 	double vx, vy; //vx and vy are about velocity in the obvious way
 	double vt; //vt is a measure of how much of the player's t coordinate is observed to elapse per unit time in the actor's reference frame
-	double dt; //this is a correction term used to measure how far off the actor's clock is from where it should be; in an ideal simulation it would be 0.
 	
 	Weapon wep;
 	BufferedImage img;
@@ -20,18 +19,12 @@ public abstract class Actor {
 
 	public void update() {
 		Player p = Main.game.player;
-		if (this == p)
-			dt = 0;
-		if (dt <= 1)
-			do {
-				x += vx;
-				y += vy;
-				for (Actor a : Main.game.actors)
-					collide(a);
-				for (Thing t : Main.game.things)
-					collide(t);
-				dt++;
-			} while (dt <= -1);
+		x += vx;
+		y += vy;
+		for (Actor a : Main.game.actors)
+			collide(a);
+		for (Thing t : Main.game.things)
+			collide(t);
 
 		if (this != p && p.dVx != 0 && p.dVy != 0)
 			updateVectors();
@@ -44,8 +37,7 @@ public abstract class Actor {
 		vt = v[0];
 		vx = v[1];
 		vy = v[2];
-		v = Lorentz.transform(dt, x, y, dVx, dVy);
-		dt = v[0];
+		v = Lorentz.transform(0, x, y, dVx, dVy);
 		x = v[1];
 		y = v[2];
 	}
