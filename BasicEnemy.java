3f4cbffda4feae4a4102d836ab55ebd3441666d0
Zane Sterling <sterling.zane@gmail.com>
2013-06-02 14:46:36 -0400

    Add BasicEnemy.

---
+++ BasicEnemy.java
@@ -0,0 +1,20 @@
+public class BasicEnemy extends Enemy {
+
+	double accel;
+
+	public BasicEnemy(double x, double y) {
+		super(x,y);
+		accel = 5;
+	}
+
+	public void update() {
+		Player p = Main.game.player;
+		double theta = Math.atan2(p.y - y, p.x - x);
+
+		vx += accel * Math.cos(theta);
+		vy += accel * Math.sin(theta);
+
+		super.update();
+	}
+
+}
