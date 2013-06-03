2481d20f50f68fa6adb9af4d3b468065bc2ab2d9
Severyn-Kozak <severyn.kozak@gmail.com>
2013-06-03 13:33:11 -0400

    Added pistol.

---
+++ Pistol.java
@@ -0,0 +1,13 @@
+public class Pistol extends Weapon{
+
+public Pistol(double x, double y){
+super(x, y);
+_RoF = 1; //dummy values here
+_ammo = 1;
+_cooldown = 1;
+}
+
+public void thingEffect(Actor a){ /*implement*/ }
+
+public void attackType(){ /*implement*/ }
+}
