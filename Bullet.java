f2d4633ee22f8f80abbc8a24239ee589eae6bf2c
Zane Sterling <sterling.zane@gmail.com>
2013-06-03 19:39:34 -0400

    Add Bullet logic.

--- Bullet.java
+++ Bullet.java
@@ -2,12 +2,15 @@ public class Bullet extends Actor{
 
 	static final int DAMAGE = 5; //dummy value
 
-	public Bullet(){ 
+	double theta;
+
+	public Bullet(double theta){ 
 		super(Main.game.player.x, Main.game.player.y); 
+		this.theta = theta;
 	}
 
-	public void thingEffect(Actor a){ 
-		a.health -= DAMAGE; 
+	public void collide(Actor a) {
+		a.takeDamage(DAMAGE);
+		die();
 	}
-
 }
