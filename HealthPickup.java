f2e6bb2849d9a6bb29e83c0393dcf638c1ef9934
Zane Sterling <sterling.zane@gmail.com>
2013-06-03 18:34:31 -0400

    Fix HealthPickup indentation.

--- HealthPickup.java
+++ HealthPickup.java
@@ -1,10 +1,10 @@
 public class HealthPickup extends Thing{
 
-public static final int HEALTH = 20;
-	
-public HealthPickup(double x, double y) {
-super(x,y);
-}
+	public static final int HEALTH = 20;
+		
+	public HealthPickup(double x, double y) {
+		super(x,y);
+	}
 
-public void thingEffect(Actor a){ a.health += HEALTH; }
+	public void thingEffect(Actor a){ a.health += HEALTH; }
 }
