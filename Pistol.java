cb5ff358c8ad6705bb7a67696d77e9fa40d12829
Zane Sterling <sterling.zane@gmail.com>
2013-06-03 19:33:09 -0400

    Add Pistol logic.

--- Pistol.java
+++ Pistol.java
@@ -1,12 +1,35 @@
-public class Pistol extends Weapon{
+import java.awt.MouseInfo;
+import java.awt.Point;
+
+public class Pistol extends Weapon {
+
+	boolean firedSinceClick;
 
 	public Pistol(){
 		super();
 		rof = 1; //dummy values here
 		ammo = 1;
 		cooldown = 10;
+
+		firedSinceClick = false;
+	}
+
+	public void attack() {
+		if (!firedSinceClick) {
+			super.attack();
+			firedSinceClick = true;
+		}
+	}
+
+	public void recover() {
+		firedSinceClick = false;
 	}
 
 	public void doAttack(){
+		Point mouseLoc = MouseInfo.getPointerInfo().getLocation();
+		Player p = Main.game.player;
+		double theta = Math.atan2(mouseLoc.y - p.y, mouseLoc.x - p.x);
+
+		Main.game.addActor(new Bullet(theta));
 	}
 }
