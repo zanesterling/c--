d05d0a76b5958badc92c29a54505e60656ce1813
Severyn-Kozak <severyn.kozak@gmail.com>
2013-06-03 23:22:36 -0400

    Add machinegun.

---
+++ MachineGun.java
@@ -0,0 +1,30 @@
+import java.awt.MouseInfo;
+import java.awt.Point;
+
+public class MachineGun extends Weapon {
+
+
+	public MachineGun(){
+		super();
+		rof = 0; //dummy values here
+		ammo = 1;
+		cooldown = 0;
+	}
+
+	public void attack() {
+		if (Main.game.mouseDown) {
+			super.attack();
+		}
+	}
+
+	public void recover() {
+		super.recover();
+	}
+
+	public void doAttack(){
+		Point mouseLoc = MouseInfo.getPointerInfo().getLocation();
+		Player p = Main.game.player;
+		double theta = Math.atan2(mouseLoc.y - Main.screen.height/2, mouseLoc.x - Main.screen.width/2);
+		Main.game.addActor(new Bullet(theta));
+	}
+}
