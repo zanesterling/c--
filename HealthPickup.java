051f6dcf35ff12e27e5bf7d016ea4d5780ae62af
Severyn-Kozak <severyn.kozak@gmail.com>
2013-06-04 05:39:25 -0400

    health

--- HealthPickup.java
+++ HealthPickup.java
@@ -1,6 +1,6 @@
 public class HealthPickup extends Thing{
 
-	public static final int HEALTH = 20;
+	public static final int HEALTH = 1;
 		
 	public HealthPickup(double x, double y){
 		super(x,y);
@@ -9,7 +9,7 @@ public class HealthPickup extends Thing{
 	public void thingEffect(Actor act){ 
 		if(act instanceof Player){
 			((Player)act).health += HEALTH; 
-			Main.screen.message = "Health +20";
+			Main.screen.message = "Health +1";
 			Main.screen.lastMsgTick = Main.game.ticks;
 		}
 	}
